45bbf08d58b8437dfb26cba0940c52331f8138a0
Thuy Duong <tttduong23@gmail.com>
2025-03-13 21:50:23 +0700

    base homepage

--- client/src/components/TitleSection.jsx
+++ client/src/components/TitleSection.jsx
@@ -1,11 +1,16 @@
-// import { Container } from "postcss";
+import PropTypes from 'prop-types';
 
-const TitleSection = () => {
-    return (
-       
-        <div>
-            <h1>Neme</h1>
-            </div>
-    );
+const TitleSection = ({ title = "Today's" }) => {
+  return (
+    <div className="flex items-center">
+      <div style={{ backgroundColor: "var(--secondary)" }} className="w-8 h-12 rounded-r-md mr-3"></div>
+      <h2 style={{ color: "var(--secondary" }}className="text-2xl font-medium text-orange-500">{title}</h2>
+    </div>
+  );
 };
-export default TitleSection ;
+
+TitleSection.propTypes = {
+  title: PropTypes.string
+};
+
+export default TitleSection;
