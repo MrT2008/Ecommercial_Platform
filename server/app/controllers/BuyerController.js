a19c09003921d7df4bcc9f91da0980decddfe968
PQuan2003 <147889621+PQuan2003@users.noreply.github.com>
2025-04-30 14:54:08 +0700

    quick fix to addProductToCart

--- server/app/controllers/BuyerController.js
+++ server/app/controllers/BuyerController.js
@@ -1,7 +1,8 @@
 
-const { where } = require('sequelize');
+const { where, json } = require('sequelize');
 const { models } = require('../models');
 const reuse = require('../reuse/reuse');
+const { response } = require('express');
 
 class BuyerController {
     viewProfileInformation = async (req, res) => {
@@ -196,38 +197,63 @@ class BuyerController {
                 return res.status(405).json({ error: 'Doesnt have enough item to add to cart' });
             }
 
+            const shop = await models.Shop.findByPk(product.shopId)
+            if (!shop) {
+                return  res.status(400).json({ error: 'failed to find shop' });
+            }
+
             const existingCart = await models.Cart.findOne({
                 where: {
                     userId: buyerId,
-                    productId: productId
+                    productId: productId,
                 }
             })
 
+            let cart = {}
             if (existingCart) {
-                const newQuantity = existingCart.quantity +  quantity
-                existingCart.update({
-                    quantity: newQuantity
-                })
+                if (existingCart.isDeleted) {
+                    existingCart.update({
+                        isDeleted: false,
+                        quantity: quantity
+                    })
+                    
+                } else {
+                    const newQuantity = existingCart.quantity +  quantity
+                    existingCart.update({
+                        quantity: newQuantity
+                    })
+                }
+                const data = {
+                    shopName: shop.name,
+                    userId: existingCart.userId,
+                    productId: existingCart.productId,
+                    productName: product.name,
+                    quantity: existingCart.quantity
+                }
+                cart = data
             } else {
-                const cart = await models.Cart.create({
+                const newCart = await models.Cart.create({
                     userId: buyerId, 
                     productId, quantity
                 }) 
-                if (!cart) {
+                if (!newCart) {
                     return res.status(400).json({ error: 'failed to add item to cart' });
                 }
+                const data = {
+                    shopName: shop.name,
+                    userId: newCart.userId,
+                    productId: newCart.productId,
+                    productName: product.name,
+                    quantity: newCart.quantity
+                }
+                cart = data
             }
             
             await product.update({
                 stock: product.stock - quantity
             })
-
-            const shop = await models.Shop.findByPk(product.shopId)
-            if (!shop) {
-                return  res.status(400).json({ error: 'failed to find shop' });
-            }
-            
-            return res.status(200).json({message: 'Add item to cart sucessfully', shopName: shop.name, product})
+    
+            return res.status(200).json({message: 'Add item to cart sucessfully', cart })
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Internal Server Error' });
@@ -259,7 +285,8 @@ class BuyerController {
                 stock: productToBeUpdated.stock + productToBeRemoved.quantity
             })
 
-            return res.status(200).json({ message: 'Product removed from cart sucessfully' });
+
+            return res.status(200).json({ message: 'Product removed from cart sucessfully', productToBeRemoved });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Internal Server Error' });
