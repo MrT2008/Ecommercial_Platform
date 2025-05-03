f81028897dbaa6e7ba083dc0d7b1436faf3c5298
Thien Ngan Le <132121395+AlexJack2502@users.noreply.github.com>
2025-05-03 13:00:22 +0700

    update Cart feature

--- server/routes/buyer.js
+++ server/routes/buyer.js
@@ -15,6 +15,7 @@ router.put('/:buyerId/payment/remove', BuyerController.removePaymentMethod) //.
 //cart
 router.post('/:buyerId/product/addToCart', BuyerController.addProductToCart) //.
 router.get('/:buyerId/cart', BuyerController.viewCart) //.
+router.put('/:buyerId/cart/update/', BuyerController.updateCart) //.
 router.put('/:buyerId/cart/remove/', BuyerController.removeProductFromCart) //.
 
 //Checkout
