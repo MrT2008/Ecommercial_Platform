93292fe30cf83464fc6085b70a34795d24f8c277
Ưng Huỳnh Phúc <phucgaming611@gmail.com>
2025-05-04 02:04:12 +0700

    update buy now, handle signup error

--- client/src/components/homeSign/signupForm.jsx
+++ client/src/components/homeSign/signupForm.jsx
@@ -38,15 +38,17 @@ const SignUpForm = () => {
         }
     
         try {
-            await signUp(userEmail, password, fullName);
-            
-            await new Promise(resolve => setTimeout(resolve, 500));
-            
-            try {
-                await login(userEmail, password);
-                setErrorMessage('');
-            } catch (error) {
-                setErrorMessage('Login failed. Please try again.');
+            const success = await signUp(userEmail, password, fullName);
+            if (success) {
+                const loginSuccess = await login(userEmail, password);
+                if (loginSuccess) {
+                    navigate('/');
+                } else {
+                    setErrorMessage('Login failed. Please try again.');
+                }
+            }
+            else {
+                setErrorMessage('Sign up failed. Due to your email is in use.');
             }
         } catch (error) {
             console.log('error is: ',error);
@@ -56,7 +58,7 @@ const SignUpForm = () => {
     useEffect(() => {
         if (session) {
             if (user?.roles.includes('manager')) {
-                navigate('/manager'); // just an example, we don't have the admin page yet
+                navigate('/admin/admin-dashboard'); // just an example, we don't have the admin page yet
             } else if (user?.roles.includes('buyer')) {
                 navigate('/');
             } else if (user?.roles.length === 0) {
